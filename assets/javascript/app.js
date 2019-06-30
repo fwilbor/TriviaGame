eb41f994e2d1b6c86cf89cfe3aa72914c21c5a4b
fwilbor <franklin.cfg@gmail.com>
2019-06-30 16:15:09 -0400

    made questions for game, Keyword Dog: Trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+var trivia = [];
+
+
+Questions
+
+WHAT IS THE NAME FOR A DOG CREATED BY CROSSING A LABRADOR RETRIEVER AND A POODLE ? (Labradoodle, Labra - Poo, Poo - Lab, DoraPoodle)
+
+
+THE CHIHUAHUA IS A BREED OF DOG BELIEVED TO ORIGINATE FROM WHAT COUNTRY ? (France, Spain, United States, Mexico)
+
+
+WHAT IS THE MOST POPULAR BREED OF DOG IN THE UNITED STATES ? (French - Bulldog, Golden Retriever, Labrador, German - Shephard)
+
+
+IN THE UNITED STATES WHICH BREED OF DOG IS COMMONLY KNOWN AS A FIREHOUSE DOG ? (Greyhound, Rottweiler, Dalmatian, Collie)
+
+
+HOW MANY CHAMBERS ARE THERE IN A DOG’S HEART ? (Four, Eight, Two, Thirteeen),
+
+    A PUGGLE IS A CROSS BETWEEN WHICH TWO DOG BREEDS ? (Pug and a Beagle, Pug and Cocker - Spaniel, Pit - bull and a Beagle, A Puggle is a Pudgie Beagle
+
+WHICH BREED OF DOG DOESNT BARK ? (Basenji, Pug, Irish - Setter, Cane - Corso)
+
+WHICH BREED OF DOG WAS SCOOBY DOO ? (St.Benard, Labrador, Great Dane, German - Shephard)

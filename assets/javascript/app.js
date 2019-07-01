fe18529f8c3bacff9ced9bdffd83a1a39d1ff675
fwilbor <franklin.cfg@gmail.com>
2019-07-01 15:29:11 -0400

    edits to JS file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,123 @@
-var trivia = [];
+$(document).ready(function () {
+
+    var pageLoad = function () {
+        $("#timer").hide();
+        $("#randomQuestion").hide();
+        $("#answers").hide();
+    }
+
+    pageLoad();
 
 
-Questions
-
-WHAT IS THE NAME FOR A DOG CREATED BY CROSSING A LABRADOR RETRIEVER AND A POODLE ? (Labradoodle, Labra - Poo, Poo - Lab, DoraPoodle)
+    var triviaBank = [{
 
 
-THE CHIHUAHUA IS A BREED OF DOG BELIEVED TO ORIGINATE FROM WHAT COUNTRY ? (France, Spain, United States, Mexico)
+        // For our Trivia Bank we created an Object inside of an Array.
+
+        question: "What is the name for a dog created by crossing a Labrador Retriever and a Poodle?",
+        answers: ["Labradoodle", "Labra - Poo", "Poo - Lab", "DoraPoodle"],
+        correctAnswer: "Labradoodle"
+    },{
+        question: "The Chihuahua is a breed of dog believed to originate from what country?",
+        answers: ["France", "Spain", "United States", "Mexico"],
+        correctAnswer: "Mexico"
+
+    },
+
+    {
+
+        question: "What is the most popular breed of dog in the United States?",
+        answers: ["French Bulldog", "Golden Retriever", "Labrador", "German Shephard"],
+        correctAnswer: "Labrador"
 
 
-WHAT IS THE MOST POPULAR BREED OF DOG IN THE UNITED STATES ? (French - Bulldog, Golden Retriever, Labrador, German - Shephard)
+    },
+
+    {
+        question: "In the United States which breed of dog is commonly knows as a Firehouse Dog?",
+        answers: ["Greyhound", "Rottweiler", "Dalmatian", "Collie"],
+        correctAnswer: "Dalmatian"
 
 
-IN THE UNITED STATES WHICH BREED OF DOG IS COMMONLY KNOWN AS A FIREHOUSE DOG ? (Greyhound, Rottweiler, Dalmatian, Collie)
+    },
+
+    {
+        question: "How many chambers are there in a dog's heart?",
+        answers: ["Four", "Eight", "Two", "Thirteeen"],
+        correctAnswer: "Four"
 
 
-HOW MANY CHAMBERS ARE THERE IN A DOG’S HEART ? (Four, Eight, Two, Thirteeen),
+    },
 
-    A PUGGLE IS A CROSS BETWEEN WHICH TWO DOG BREEDS ? (Pug and a Beagle, Pug and Cocker - Spaniel, Pit - bull and a Beagle, A Puggle is a Pudgie Beagle
+    {
+        question: "A Puggle is a cross between which two dog breeds?",
+        answers: ["Pug and Cocker Spaniel", "Pug and a Beagle", "Pitbull and a Beagle", "A Puggle is a Pudgie Beagle"],
+        correctAnswer: "Pug and a Beagle"
 
-WHICH BREED OF DOG DOESNT BARK ? (Basenji, Pug, Irish - Setter, Cane - Corso)
 
-WHICH BREED OF DOG WAS SCOOBY DOO ? (St.Benard, Labrador, Great Dane, German - Shephard)
+    },
+
+    {
+        question: "Which breed of dog doesn't bark?",
+        answers: ["Pug", "Irish Setter", "Cane Corso", "Basenji"],
+        correctAnswer: "Basenji"
+
+
+    },
+
+    {
+        question: "Which breed of dog was Scooby Doo?",
+        answers: ["St.Benard", "Great-Dane", "Labrador", "German-Shephard"],
+        correctAnswer: "Great-Dane"
+
+    }];
+
+
+    var gifArray = ["question1", "question2", "question3", "question4", "question5", "question6", "question7", "question8"];
+
+    var currentQuestion;
+    var correctAnswer;
+    var incorrectAnswer;
+    var unanswered;
+    var seconds;
+    var time;
+    var answered;
+    var userSelect;
+    var messages = {
+        correct: "You got it right, Totally!",
+        incorrect: "No, that's Totally wrong.",
+        endTime: "You're Out of time!",
+        finished: "Alright! Let's see how you did."
+
+    }
+
+
+    // Next we create a Start Button to Activate Game when Clicked
+    // Player is Given a Random Trivia Question and Timer begins
+    // If Player guesses right answer before time expires they go to the next question
+    // Else player gets incorrect. Time Resets on each Question 
+    // 4 Correct Answers wins the Game
+
+    //Start Game Function
+
+    // var startGame = funtion() {
+
+    // };
+
+
+    // var answerCheck = function() {
+
+    // };
+
+
+    // var results = function() {
+
+
+    // };
+
+
+    // $(document).on("click", "#start", function () {
+    //     console.log('Start button being clicked!!');
+    //     $("#randomQuestion").append("<h4>Questions from object to show here randomly (looped)</h4>");
+        
+    // }
